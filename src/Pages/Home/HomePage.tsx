40edb37a5a76fcb9374d0b4c4cdb37b4319d2aca
lisicyn <lisicyn@kribrum.ru>
2024-04-23 00:08:01 +0300

    feat(homePage): first attempt of displaying data from request to /me

--- src/Pages/Home/HomePage.tsx
+++ src/Pages/Home/HomePage.tsx
@@ -1,5 +1,24 @@
+import { useMeQuery } from "../../services/api";
+import { IMe } from "../../services/constants";
+import { Spinner } from "../../Components/Loaders/Spinner";
+
 export const HomePage = () => {
+    const { data, isError, isLoading, isSuccess, refetch } = useMeQuery();
+
+    if (isLoading) {
+        return <Spinner width="w-1/4"/>
+    }
+
+    //TODO https://lxadm.com/type-undefined-is-not-assignable-to-type/
+    //Необходимо решить проблему того, что тс видит возможность получения undefined
+    const { img, login, name }: IMe = data;
+
     return (
-        <h1 className="font-bold">Home</h1>
+        <div>
+            <h1 className="font-bold">Home</h1>
+            <img src={img}/>
+            <p>{login}</p>
+            <p>{name}</p>
+        </div>
     )
 }
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import servicesConstants, { IUser } from './constants';
+import servicesConstants, { IMe, IUser } from './constants';
 
-const { reducerPath, baseUrl, register } = servicesConstants;
+const { reducerPath, baseUrl, register, me } = servicesConstants;
 
 export const authApi = createApi({
   reducerPath: reducerPath,
@@ -13,10 +13,30 @@ export const authApi = createApi({
         url: register,
         body: body
       })
-    })
+    }),
+    me: builder.query<IMe, void>({
+      query: () => ({
+        url: me,
+        prepareHeaders: (headers: Headers) => {
+            const accessToken = localStorage.getItem('Token');
+        
+            if (accessToken) {
+              headers.set('Authorization', `Bearer ${accessToken}`);
+            }
+        
+            headers.set('Accept', 'application/json')
+            headers.set('Cache-Control', 'no-cache')
+            headers.set('Pragma', 'no-cache')
+            headers.set('Expires', '0')
+        
+            return headers
+        }
+      })
+    }),
   })
 });
 
 export const {
-  useRegisterMutation
+  useRegisterMutation,
+  useMeQuery
 } = authApi;
