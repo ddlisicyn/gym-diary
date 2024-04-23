dac0e2252b74402e6295875039f1ea0ad47e87d5
lisicyn <lisicyn@kribrum.ru>
2024-04-23 12:36:38 +0300

    feat(homePage): header layout with link to profile

--- src/Pages/Home/HomePage.tsx
+++ src/Pages/Home/HomePage.tsx
@@ -1,6 +1,7 @@
 import { useMeQuery } from "../../services/api";
 import { IMe } from "../../services/constants";
 import { Spinner } from "../../Components/Loaders/Spinner";
+import { Link } from "react-router-dom";
 
 export const HomePage = () => {
     const { data, isError, isLoading, isSuccess, refetch } = useMeQuery();
@@ -14,11 +15,9 @@ export const HomePage = () => {
     const { img, login, name }: IMe = data;
 
     return (
-        <div>
-            <h1 className="font-bold">Home</h1>
-            <img src={img}/>
-            <p>{login}</p>
-            <p>{name}</p>
+        <div className="flex flex-row justify-between w-screen p-10 items-center">
+            <p className="font-semibold text-lg/8">Привет, {name}</p>
+            <Link to="profile"><img src={img} alt="profile-avatar" className="w-10 rounded-full"/></Link>
         </div>
     )
 }
